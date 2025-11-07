f6a5af2b3b3181a2c97a5d4c315ba197d757eabb
Tanuj <116993813+tanujbtechcse2022@users.noreply.github.com>
2025-11-07 17:17:20 +0530

    Add files via upload

---
+++ Table of a number.cpp
@@ -0,0 +1,12 @@
+//print table of a number
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number: ";
+	cin>>n;
+	for(int i=1;i<=10;i++){
+		cout<<n<<" * "<<i<<" = "<<n*i;
+		cout<<endl;
+	}
+}
