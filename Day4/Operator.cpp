755722ad7f9bd6a2b9d8acf386e7a54ad5a0455a
Tanuj <116993813+tanujbtechcse2022@users.noreply.github.com>
2025-11-10 22:09:53 +0530

    Add files via upload

---
+++ Day4/Operator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//Logical Operator
+//	int a,b,c;
+//	cin>>a>>b>>c;
+//	
+//	if(a>b&&a>c)
+//	cout<<"yes";
+//	 else
+//	 cout<<"no";
+
+
+//or
+
+
+//char name='i';
+//if(name=='a'||name=='e'||name=='i'|name=='o'|name=='u')
+//cout<<"vowel";
+//else
+//cout<<"consonant";
+
+
+//not
+
+
+
+cout << (!123) << endl;
+    cout << (!0);
+    return 0;
+}
