c6264ed4ac5a33d2595ce551fd8a662163dd063c
Tanuj <116993813+tanujbtechcse2022@users.noreply.github.com>
2025-11-29 16:35:11 +0530

    Add files via upload

---
+++ Day10/fibonacci_no_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number: ";
+	cin>>n;
+	int arr[1000];
+	//first no.->0
+	//second no. -> 1
+	arr[0]=0;
+	arr[1]=1;
+	
+	for(int i=2;i<=n-1;i++)
+	arr[i]=arr[i-1]+arr[i-2];
+	
+	cout<<arr[n-1];
+}
