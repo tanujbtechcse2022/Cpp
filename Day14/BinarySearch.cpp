aeb783deeaf09db18fd1120aa33f479b8e1df774
Tanuj <116993813+tanujbtechcse2022@users.noreply.github.com>
2025-12-04 23:22:18 +0530

    Add files via upload

---
+++ Day14/BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+	int BinarySearch(int arr[],int size,int key){
+		int start=0,end=size-1,mid;
+		while(start<=end){
+			mid=(start+end)/2;
+			if(arr[mid]==key)
+			return mid;
+			
+			else if(arr[mid]<key)
+			start = mid+1;
+			
+			else
+			end=mid-1;
+		}
+		return -1;
+	}
+	
+	int main(){
+	int arr[100];
+	int size;
+	cout<<"Enter the size: ";
+	cin>>size;
+	
+	cout<<"Enter the array: ";
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+	}
+	
+	int key;
+	cout<<"Enter the key: ";
+	cin>>key;
+	
+	cout<<BinarySearch(arr,size,key)<<endl;
+	return 0;
+}
